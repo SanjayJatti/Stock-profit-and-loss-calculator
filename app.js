b04b4357dd1f4dde5d8033e45d03011275610093
SanjayJatti <jattisanjay.r@gmail.com>
2021-09-13 10:31:19 -0700

    fixed logic and add footer

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var checkBtn = document.querySelector("#check-btn");
 var outputEl = document.querySelector("#output");
 
 function calculateProfitAndLoss(initial,qty,current){
+    
+    if (initial <= 0 || qty <= 0 || current <= 0) {
+        return outputEl.innerText = "Please enter valid inputs";
+    }
     if(current > initial){
         var profit = (current - initial)*qty;
-        var profitPercentage = (profit/initial)*100;
+        var profitPercentage = (profit/(initial*qty))*100;
         outputMsg(`Yay! You are in profit😍.The Profit is ${profit.toFixed(2)} & the profit percentage is ${profitPercentage.toFixed(2)}%`)
         outputEl.style.background="green";
     }else if(initial > current){
@@ -16,7 +20,7 @@ function calculateProfitAndLoss(initial,qty,current){
         outputMsg(`Oops! You are in loss 😌. The loss is ${loss.toFixed(2)} and the loss percentage is ${lossPercentage.toFixed(2)}%`);
         outputEl.style.background="red";
     }else{
-        outputMsg("No Pain, No gain and No gain, No pain!🙂");
+        outputMsg("No gain, No pain! means No profit or no loss yet! 🙂");
         outputEl.style.color="black";
     }
 };
@@ -24,13 +28,7 @@ function clickHandler(){
     var ip = Number(initialPrice.value);
     var sq = Number(stockQty.value);
     var curr = Number(currentPrice.value);
-
-    if(ip && sq && curr){
-        calculateProfitAndLoss(ip,sq,curr);
-    }else{
-        outputMsg("Please enter all valid input")
-    }
- 
+    calculateProfitAndLoss(ip,sq,curr);
 };
 
 function outputMsg(msg){
